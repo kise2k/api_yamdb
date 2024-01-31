5f302972482d9441758d991d7eb751ccde144bc0
Ким Данил <danil.kim.2001@bk.ru>
2024-01-31 22:38:10 +0300

    new_save_1

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -9,28 +9,22 @@ class IsAdmin(permissions.BasePermission):
             user.is_admin or user.is_staff or user.is_superuser
         )
 
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        return user.is_authenticated and (
-            user.is_admin or user.is_staff or user.is_superuser
-        )
-
 
 class IsAthorModeraterAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.method == 'POST':
-            return user.is_authenticated
-        return user.is_authenticated and (
-            user == obj.author
+        
+        return (
+            request.method in permissions.SAFE_METHODS
+            or user.is_authenticated
+            and obj.author == user
             or user.is_admin
             or user.is_moderator
             or user.is_staff
             or user.is_superuser
         )
+        
 
 
 class ReadOnly(permissions.BasePermission):
